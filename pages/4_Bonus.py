bb2304c9ce513ee7ded8cd8653c8614537250f01
YahyaBouayad <144125888+YahyaBouayad@users.noreply.github.com>
2023-11-30 18:32:40 +0100

    Update 4_Bonus.py

---
+++ pages/4_Bonus.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+from fonction_project import *
+from fonction_ml import *
+
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import mean_squared_error,r2_score
+
+
+data=st.session_state["data"]
+drogues_col=st.session_state['drogues_col']
