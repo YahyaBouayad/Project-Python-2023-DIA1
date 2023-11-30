b0a443a9d08718dc8694d1959ecb6878648151db
YahyaBouayad <144125888+YahyaBouayad@users.noreply.github.com>
2023-11-30 01:58:55 +0100

    Update 3_Machine_Learning.py

--- pages/3_Machine_Learning.py
+++ pages/3_Machine_Learning.py
@@ -24,16 +24,31 @@ from sklearn.metrics import mean_squared_error,r2_score
 
 
 data=st.session_state["data"]
+drogues_col=st.session_state['drogues_col'] 
+
+models = { ' Support Vector Machines': SVC(),
+            ' Decision Tree Classifier': DecisionTreeClassifier(),
+            'Random Forest Classifier': RandomForestClassifier(),}
 
 st.title("Machine Learning work:")
 st.markdown("The objectif of our Machin learning training is to determine if we can find is someone use or used one of the drugs using her Information and his caracter. ")
 name_features=['Age','Genre', 'Education','Névrotique', 'Extraverti', 'Ouvert à l\'expérience', 'Amical', 'Consciencieux', 'Imuplsif', 'En recherche de sensations']
-
-
-
+name_target=st.selectbox('Choose a drug to train', drogues_col)
 features = data[name_features]  # Ajoutez ici d'autres caractéristiques pertinentes
-target = data['Consommation de cannabis']
-prediction_training(features,target,SVC())
+target = data[name_target]
 
 
 
+tab1, tab2, tab3 = st.tabs(["Support Vector Machines", "Decision Tree Classifier","Random Forest Classifier"])
+
+with tab1:
+    plt_1,y_pred_1,report_1,accuracy_1=prediction_training(features,target,SVC())
+    st.pyplot(plt_1)
+    
+with tab2:
+    plt_2,y_pred_2,report_2,accuracy_2=prediction_training(features,target,DecisionTreeClassifier())
+    st.pyplot(plt_2)
+    
+with tab3:
+    plt_3,y_pred_3,report_3,accuracy_3=prediction_training(features,target,RandomForestClassifier())
+    st.pyplot(plt_3)
