f84fe24ecbd50668d3dc03c0f63121571b8b3883
YahyaBouayad <144125888+YahyaBouayad@users.noreply.github.com>
2023-11-30 21:56:35 +0100

    Update fonction_ml.py

--- fonction_ml.py
+++ fonction_ml.py
@@ -190,7 +190,7 @@ def prediction_training_f(data,data_final,model,drug):
     y_pred = model.predict(data_final)
     return y_pred
     
-def process_user_input(age, genre, education, neuroticisme, extraversion, exp, amicalite, conscience, impulsivite, recherche, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16, d17, d18):
+def process_user_input(data,age, genre, education, neuroticisme, extraversion, exp, amicalite, conscience, impulsivite, recherche, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16, d17, d18):
     # Créer un DataFrame vide avec les mêmes colonnes que data2
     user_data = pd.DataFrame(columns=data.columns)
 
