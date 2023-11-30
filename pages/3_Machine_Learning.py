4df775bca01a34d0260b0fcc8d18f4a23f91a2d2
YahyaBouayad <144125888+YahyaBouayad@users.noreply.github.com>
2023-11-30 02:15:44 +0100

    Update 3_Machine_Learning.py

--- pages/3_Machine_Learning.py
+++ pages/3_Machine_Learning.py
@@ -38,8 +38,9 @@ features = data[name_features]  # Ajoutez ici d'autres caractéristiques pertine
 target = data[name_target]
 
 st.markdown("The data features are :")
-st.markodwn("'Age','Genre', 'Education','Névrotique', 'Extraverti', 'Ouvert à l\'expérience', 'Amical', 'Consciencieux', 'Imuplsif' and 'En recherche de sensations'")
-
+"""
+'Age','Genre', 'Education','Névrotique', 'Extraverti', 'Ouvert à l\'expérience', 'Amical', 'Consciencieux', 'Imuplsif', 'En recherche de sensations'
+"""
 st.markdown("The data Target is :")
 st.write(name_target)
 
