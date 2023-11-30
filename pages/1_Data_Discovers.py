1f0ab20ece1d109987c6a0912948c372c0bcb5b0
YahyaBouayad <144125888+YahyaBouayad@users.noreply.github.com>
2023-11-30 01:07:19 +0100

    Update 1_Data_Discovers.py

---
+++ pages/1_Data_Discovers.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+from fonction_project import *
+from fonction_ml import *
+info_col = [
+    'Age', 
+    'Genre', 
+    'Education', 
+    'Pays',
+    'Ethnie',
+]
+
+caract_col = [
+    'Neuroticisme',
+    'Extraversion',
+    'Ouverture à l\'expérience',
+    'Amicalité',
+    'Conscience',
+    'Impulsivité',
+    'Recherche de sensations'
+]
+
+personne_col = info_col + caract_col
+
+drogues_col = [
+    'Consommation d\'alcool',
+    'Consommation d\'amphétamines',
+    'Consommation d\'amyl',
+    'Consommation de benzodiazepine',
+    'Consommation de café',
+    'Consommation de cannabis',
+    'Consommation de chocolat',
+    'Consommation de cocaïne',
+    'Consommation de crack',
+    'Consommation d\'ecstasy',
+    'Consommation d\'héroïne',
+    'Consommation de ketamine',
+    'Consommation de drogues légales',
+    'Consommation de LSD',
+    'Consommation de meth',
+    'Consommation de champignons magiques',
+    'Consommation de nicotine',
+    'Consommation de Semeron',
+    'Consommation de VSA'
+]
+
+drogues_autorisées = ['Consommation d\'alcool', 'Consommation de café', 'Consommation de chocolat', 'Consommation de nicotine']
+drogues_illégales = [i for i in drogues_col if i not in drogues_autorisées]
+col = personne_col + drogues_col
+
+data=lecture_data()
+pers_data=pre_data(data,col,drogues_col,drogues_illégales)
