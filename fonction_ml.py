bcb0758889b478c783dc30b82b950a54b671c218
YahyaBouayad <144125888+YahyaBouayad@users.noreply.github.com>
2023-11-30 21:39:01 +0100

    Update fonction_ml.py

--- fonction_ml.py
+++ fonction_ml.py
@@ -18,7 +18,7 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.metrics import mean_squared_error,r2_score
 import warnings
 warnings.filterwarnings('ignore')
-
+drogues_col=st.session_state['drogues_col']
 def prediction_training(features,target,model):
     
     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
