a0e2dd8fa5daddc3e04743ef7a3b343dc54d7f98
YahyaBouayad <144125888+YahyaBouayad@users.noreply.github.com>
2023-11-30 21:52:12 +0100

    Update 2_Data_Analyse.py

--- pages/2_Data_Analyse.py
+++ pages/2_Data_Analyse.py
@@ -10,7 +10,10 @@ from fonction_ml import *
 import warnings
 warnings.filterwarnings('ignore')
 
-
+data=st.session_state["data"]
+drogues_col=st.session_state['drogues_col'] 
+pers_data=st.session_state["pers_data"]
+caract_col=st.session_state["caract_col"]
 st.title("Let's get in the details :")
 st.markdown("Radar graph that highlights the characteristics of the average person that consumes :")
 selected_column_drogue = st.selectbox('Choose a column', drogues_col )
