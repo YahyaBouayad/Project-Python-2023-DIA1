af4150a7014cc8d741fd18f55a99cd1321258a20
YahyaBouayad <144125888+YahyaBouayad@users.noreply.github.com>
2023-11-30 01:10:20 +0100

    Update 1_Data_Discovers.py

--- pages/1_Data_Discovers.py
+++ pages/1_Data_Discovers.py
@@ -5,8 +5,19 @@ import plotly.express as px
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import seaborn as sns
-from fonction_project import *
-from fonction_ml import *
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import mean_squared_error,r2_score
+import warnings
+warnings.filterwarnings('ignore')
 info_col = [
     'Age', 
     'Genre', 
